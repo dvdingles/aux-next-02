e9e0fad9c1c4df92d5f2f24f2cb37a17a2162708
David Inglés <dvdingles@hotmail.com>
2022-12-07 20:49:01 +0100

    todo va más o menos

---
+++ pages/components/inicio/Inicio.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Inicio.module.css'
+
+const Inicio = () => {
+  return (
+    <>
+         <h1 className={styles.cartel}>Auxiliar Manipuladora <br />del Cartón</h1>
+    </>
+  )
+}
+
+export default Inicio
---
+++ pages/components/layout/Layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import styles from './Layout.module.css'
+
+const Layout = () => {
+  return (
+    <>
+        <header className={styles.cartel}>
+            <img className={styles.logo} src='/logo-aux.svg' alt='david'></img>
+           
+        </header>
+    </>
+  )
+}
+
+export default Layout
